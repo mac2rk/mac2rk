6a7cf79a985258c194839dad35dd19cd36546e46
Alexey Khudyakov <alexcp@me.com>
2012-10-16 15:08:45 +0400

    Initial Commit

---
+++ mac2rk/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mac2rk
+//
+//  Created by Alexey Khudyakov on 10/16/12.
+//  Copyright (c) 2012 alexcp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
