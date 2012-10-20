899182d2d55a28d31c009131f1e8d3107bf7de39
Alexey Khudyakov <alexcp@me.com>
2012-10-20 14:11:59 +0400

    initial commit, no audio functionality

--- mac2rk/main.c
+++ mac2rk/main.c
@@ -7,12 +7,80 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[])
+void usage(void)
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+	printf("Usage: mac2rk filename [baudrate]\n");
+	exit (8);
 }
 
+int main(int argc, char *argv[])
+{
+    long baudRate = 1280;
+    char *sourceFileName;
+    FILE *sourceFile;
+    long fileSize;
+    char *buffer, *ptr;
+    int ch, i;
+    
+    switch (argc) {
+        case 3:
+            baudRate = atol(argv[2]);
+        case 2:
+            sourceFileName = argv[1];
+            break;
+        default:
+            usage();
+            return EXIT_FAILURE;
+    }
+
+    sourceFile = fopen(sourceFileName, "rb");
+    
+    if (sourceFile == NULL) {
+        printf("Cannot open source file %s\n", sourceFileName);
+        exit(EXIT_FAILURE);
+    }
+    
+    fseek(sourceFile, 0L, SEEK_END);
+    fileSize = ftell(sourceFile);
+    fseek(sourceFile, 0L, SEEK_SET);
+    
+    if (fileSize > 0x7600L) {
+        printf("Source file is too long\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    buffer = (char *)malloc(fileSize*sizeof(char));
+    
+    if (buffer == NULL) {
+        printf("Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    ptr = buffer;
+    while ((ch = fgetc(sourceFile)) != EOF && (ptr - buffer) < fileSize) *ptr++ =ch;
+
+    
+
+    
+    ptr = buffer;
+    while ((ptr - buffer) < fileSize) {
+        ch = *ptr++;
+        for (i = 7; i >= 0; i--) {
+            if (ch & (1 << i)) {
+                printf("1");
+            } else {
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
+    
+
+    
+    free (buffer);
+    fclose(sourceFile);
+    
+    return 0;
+}
